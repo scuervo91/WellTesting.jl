0f4f15fbbecb89781b564f13870202f1c58c3158
Santiago <scuervo91@gmail.com>
2019-07-15 22:20:54 -0500

    Files generated by PkgTemplates

---
+++ src/WellTesting.jl
@@ -0,0 +1,5 @@
+module WellTesting
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using WellTesting
+using Test
+
+@testset "WellTesting.jl" begin
+    # Write your own tests here.
+end
